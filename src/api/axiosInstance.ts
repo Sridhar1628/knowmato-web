893dded6666c4121330097ab48d71504d5b72cdb
Sridhar <sridharmoorthi2828@gmail.com>
2026-05-30 17:21:30 +0530

    second update

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://kno/api",
+  baseURL: "https://api.knowmato.in/api",
   timeout: 30000,
   headers: {
     "Content-Type": "application/json",
